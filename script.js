86cf7b917b30b214fc9b76327aff357f3bd38bb7
Roberto Nunes Ferreira <rrobertonunesferreira@gmail.com>
2024-09-11 01:27:57 -0300

    filtro finalizado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const containerPokemon = document.querySelector('.pokedex');
+const barraDePesquisa = document.querySelector('.pesquisar__input');
 
 async function buscarEMostrarPokemon() {
     try {
@@ -10,10 +11,10 @@ async function buscarEMostrarPokemon() {
             const pokemonTypes = pokemon.type2 ? `${pokemon.type1}/${pokemon.type2}` : pokemon.type1;
                 containerPokemon.innerHTML += `
                 <div class="card ${cardTypes}">
-                    <h3>#${pokemon.number}</h3>
+                    <h3 class="card__number">${pokemon.number}</h3>
                     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.number}.png" alt="${pokemon.name}">
-                    <h2>${pokemon.name}</h2>
-                    <p>${pokemonTypes}</p>
+                    <h2 class="card__name">${pokemon.name}</h2>
+                    <p class="card__type">${pokemonTypes}</p>
                 </div>
                 `;   
         });
@@ -24,3 +25,20 @@ async function buscarEMostrarPokemon() {
 }
 
 buscarEMostrarPokemon();
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa () {
+    const pokemons = document.querySelectorAll('.card');
+    const filtro = barraDePesquisa.value.toLowerCase();
+
+    pokemons.forEach((pokemon) => {
+        const name = pokemon.querySelector('.card__name').textContent.toLowerCase();
+        const number = pokemon.querySelector('.card__number').textContent;
+        const type = pokemon.querySelector('.card__type').textContent.toLowerCase();
+
+        const corresponde = name.includes(filtro) || number.includes(filtro) || type.includes(filtro);
+        
+        pokemon.style.display = corresponde ? 'block': 'none';
+    })
+}
